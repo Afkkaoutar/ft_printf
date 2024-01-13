f23847f380c0fd463e2abc27aa96951ca35423ca
kaafkhar <afkharkaoutar18@gmail.com>
2024-01-13 10:44:16 +0100

    donr

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,19 @@
 /*   By: kaafkhar <kaafkhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 08:20:05 by kaafkhar          #+#    #+#             */
-/*   Updated: 2024/01/07 23:57:48 by kaafkhar         ###   ########.fr       */
+/*   Updated: 2024/01/13 04:45:28 by kaafkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	check(char c)
+{
+	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i'
+		|| c == 'u' || c == 'x' || c == 'X' || c == '%')
+		return (1);
+	return (0);
+}
 
 int	ft_printf1(char f, va_list pt)
 {
@@ -56,7 +64,7 @@ int	ft_printf(const char *c, ...)
 			break ;
 		if (c[i] != '%')
 			len += ft_putchar(c[i]);
-		else if (c[i] == '%')
+		else if (c[i] == '%' && check(c[i + 1]) == 1)
 		{
 			len += ft_printf1(c[i + 1], l);
 			i++;
